91df02cf6aaecf452cc93f8a7395d676648f20c5
Ahmed Ramadan <143494954+ahmediramadan01@users.noreply.github.com>
2024-09-15 15:00:59 +0300

    Add opening and closing dialog functionality

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 "use strict";
+
+const dialogElement = document.querySelector(".dialog");
+const openDialogButtonElement = document.querySelector(".button--open");
+const closeDialogButtonElement = document.querySelector(".button--close");
+
+openDialogButtonElement.addEventListener("click", () => {
+	dialogElement.showModal();
+});
+closeDialogButtonElement.addEventListener("click", () => {
+	dialogElement.close();
+});
